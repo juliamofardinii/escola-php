b37c3353242d5fabc50dbc9e1cae0729de0498c7
Júlia Mofardini <150747679+juliamofardinii@users.noreply.github.com>
2024-04-28 20:10:09 -0300

    iniciar projeto

---
+++ aluno.php
@@ -0,0 +1,78 @@
+<?php
+$conexao = mysqli_connect("localhost:3306", "root", "root", "escola");
+if ($conexao->connect_error) {
+    die("Connection failed: " . $conexao->connect_error);
+}
+
+if (isset($_POST['submit'])) {
+
+    $nome = $_POST['nome'];
+    $datanascimento = $_POST['datanascimento'];
+    $cpf = $_POST['cpf'];
+
+    $sql = "INSERT INTO alunos (NOME,DATANASCIMENTO,CPF) VALUES('$nome', '$datanascimento', '$cpf')";
+
+    $result = mysqli_query($conexao, $sql);
+}
+
+$sql = "SELECT * FROM alunos";
+
+$alunos = mysqli_query($conexao, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aluno</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Data de nascimento</th>
+                <th>Cpf</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($aluno = mysqli_fetch_assoc($alunos)) {
+                echo "<tr>";
+                echo "<td>" . $aluno['id'] . "</td>";
+                echo "<td>" . $aluno['nome'] . "</td>";
+                echo "<td>" . $aluno['datanascimento'] . "</td>";
+                echo "<td>" . $aluno['cpf'] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <br />
+    <form action="aluno.php" method="POST">
+        <div>
+            <label>Nome</label>
+            <input type="text" id="nome" name="nome" value="" autofocus class="form-control" require />
+        </div>
+        <div>
+            <label>Data de nascimento</label>
+            <input type="date" name="datanascimento" value="" class="form-control" require />
+        </div>
+        <div>
+            <label>Cpf</label>
+            <input type="number" name="cpf" value="" class="form-control" require />
+        </div>
+
+        <br>
+        <div>
+            <input type="submit" name="submit" value="cadastrar" />
+        </div>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ turma.php
@@ -0,0 +1,78 @@
+<?php
+$conexao = mysqli_connect("localhost:3306", "root", "root", "escola");
+if ($conexao->connect_error) {
+    die("Connection failed: " . $conexao->connect_error);
+}
+
+if (isset($_POST['submit'])) {
+
+    $descricao = $_POST['descricao'];
+    $ano = $_POST['ano'];
+    $vagas = $_POST['vagas'];
+
+    $sql = "INSERT INTO turmas (descricao, ano, vagas) VALUES('$descricao', '$ano', '$vagas')";
+
+    $result = mysqli_query($conexao, $sql);
+}
+
+$sql = "SELECT * FROM turmas";
+
+$turmas = mysqli_query($conexao, $sql);
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turmas</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Descrição</th>
+                <th>Ano</th>
+                <th>Vagas</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($turma = mysqli_fetch_assoc($turmas)) {
+                echo "<tr>";
+                echo "<td>" . $turma['ID'] . "</td>";
+                echo "<td>" . $turma['descricao'] . "</td>";
+                echo "<td>" . $turma['ano'] . "</td>";
+                echo "<td>" . $turma['vagas'] . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+    <br />
+    <form action="turma.php" method="POST">
+        <div>
+            <label>Descrição</label>
+            <input type="text" id="descricao" name="descricao" value="" autofocus class="form-control" require />
+        </div>
+        <div>
+            <label>Ano</label>
+            <input type="number" name="ano" value="" class="form-control" require />
+        </div>
+        <div>
+            <label>Vagas</label>
+            <input type="number" name="vagas" value="" class="form-control" require />
+        </div>
+
+        <br>
+        <div>
+            <input type="submit" name="submit" value="Cadastrar" />
+        </div>
+        </div>
+    </form>
+</body>
+
+</html>
