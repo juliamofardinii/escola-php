9cbfaa9e390bdd515cf97e13dddc1b9753c1df0d
Júlia Mofardini <150747679+juliamofardinii@users.noreply.github.com>
2024-04-29 11:23:38 -0300

    alteração css

--- index.php
+++ index.php
@@ -4,29 +4,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- GOOGLE FONTES -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <!-- FIM GOOGLE FONTES -->
-    <!--BOOTSTRAP ICONS-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <!--FIM BOOTSTRAP ICONS-->
-    <link rel="stylesheet" href="style.css">
     <title>Escola</title>
+    <link rel="stylesheet" href="style.css>
 </head>
 
 <body>
-    <header>
-        <div class="interface">
-            <nav class="menu-desktop">
-                <ul>
-                    <li><a href="/aluno.php">Alunos</a></li>
-                    <li><a href="/turma.php">Turmas</a></li>
-                </ul>
-            </nav>
-        </div><!--interface-->
+    <header class="cabecalho">
+        <nav class="cabecalho__menu">
+            <a class="cabecalho__menu__link" href="aluno.php">Alunos</a>
+            <a class="cabecalho__menu__link" href="turma.php">Turmas</a>
+        </nav>
     </header>
+    <main class="apresentacao"> 
+        <img src="./assets/imagem.png" alt="">
+    </main>
+    <footer class="rodape">
+    </footer>
 </body>
 
 </html>
